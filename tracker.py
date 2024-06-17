70019b460e65dc8bd3fb01170b71f2f13fb2edb8
Mayuran Thanushan <mayuranthanushan@gmail.com>
2024-06-17 17:23:01 +0530

    initial commit

---
+++ tracker.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+
+    while True:
+        print("Welcome to the Expense Tracker!")
+        print("\n---- Menu ----")
+        print("1. Add Expense" +
+        "\n2. Add Payment" +
+        "\n3. Display Expenses" +
+        "\n4. Display shares" +
+        "\n5. Save Data" +
+        "\n6. Load Data" +
+        "\n7. Exit")
+
+        choice = input("\nChoose an option: ")
