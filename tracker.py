a9a5d35b162ee3ccdea0c15455bc92d2281ac718
Mayuran Thanushan <mayuranthanushan@gmail.com>
2024-06-17 18:28:27 +0530

    added choices

--- tracker.py
+++ tracker.py
@@ -1,7 +1,11 @@
+expenses = []
+payment = []  
+
+
 if __name__ == "__main__":
+    print("Welcome to the Expense Tracker!")
 
     while True:
-        print("Welcome to the Expense Tracker!")
         print("\n---- Menu ----")
         print("1. Add Expense" +
         "\n2. Add Payment" +
@@ -12,3 +16,31 @@ if __name__ == "__main__":
         "\n7. Exit")
 
         choice = input("\nChoose an option: ")
+
+        if choice == '1':
+            name = input("Enter the person name: ")
+            amount = float(input("Enter expense amount: "))
+            description = input("Enter expense description: ")
+            add_expense(name, amount, description)
+        elif choice == '2':
+            name = input("Enter the person name: ")
+            amount = float(input("Enter payment amount: "))
+            description = input("Enter payment description: ")
+            add_payment(name, amount, description)
+        elif choice == '3':
+            name = input("Enter the person name (type \"all\" to print all): ")
+            display_expenses(name)
+        elif choice == '4':
+            name = input("Enter the person name (type \"all\" to print all): ")
+            display_shares(name)
+        elif choice == '5':
+            filename = input("Enter filename to save data: ")
+            save_data(filename)
+        elif choice == '6':
+            filename = input("Enter filename to load data: ")
+            load_data(filename)
+        elif choice == '7':
+            print("Exiting Expense Tracker!")
+            break
+        else:
+            print("Invalid choice, please try again.")
